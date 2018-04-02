a1128d953f1ed64d4d1ff027dc33a1c905871d65
Evilermine <35575424+Evilermine@users.noreply.github.com>
2018-04-01 20:35:52 -0400

    created UserProfile

---
+++ module/User/src/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace User\Models;
+
+class User{
+    private $userId;
+    private $username;
+    private $passwordHash;
+    private $email;
+}
+
+?>
---
+++ module/User/src/Models/UserProfile.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace User\Models;
+
+class UserProfile{
+    private $firstname;
+    private $lastname;
+    private $address;
+    private $city;
+    private $country;    
+}
+?>
