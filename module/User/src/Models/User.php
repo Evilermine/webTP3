254538e57260956716df5097be808a06bcc98229
p3miki <p3miki@outlook.com>
2018-04-01 23:40:16 -0400

    Work on User module. Cannot resolve AuthController::class?

--- module/User/src/Models/User.php
+++ module/User/src/Models/User.php
@@ -3,10 +3,29 @@
 namespace User\Models;
 
 class User{
-    private $userId;
-    private $username;
-    private $passwordHash;
-    private $email;
+    public $_username;
+    public $_salt;
+    public $_hashedPass;
+
+    public function __construct() 
+    {
+
+    }
+
+    public function exchangeArray($data)
+    {
+        $this->_username = (!empty($data['username'])) ? $data['username'] : null;
+        $this->_salt = (!empty($data['salt'])) ? $data['salt'] : null;
+        $this->_hashedPass = (!empty($data['hashedPass'])) ? $data['hashedPass'] : null;
+    }
+
+    public function toValues(){
+        return [
+            'username' => $this->_username,
+            'salt' => $this->_salt,
+            'hashedPass' => $this->_hashedPass
+        ];
+    }
 }
 
 ?>
